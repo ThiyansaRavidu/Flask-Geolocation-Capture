bdab266229a445d53d3e2601c1b2fdb96f8c2166
TRN <108600032+ThiyansaRavidu@users.noreply.github.com>
2025-07-04 19:23:26 +0530

    app.py

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ def panel():
     return render_template("panel.html", entries=entries, link=url_for("index", _external=True))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
